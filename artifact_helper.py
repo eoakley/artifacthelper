2e9a4b2804c52f6b1ecf7c68bdc0f2b92cf56232
Jota <joaopvcs@gmail.com>
2018-12-08 05:07:01 -0200

    Settings file added, overlay remembers position and auto-scan after reloading program , minor fixes.

--- artifact_helper.py
+++ artifact_helper.py
@@ -14,6 +14,8 @@ import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
 from mss import mss
+import atexit
+
 
 path_root = os.path.dirname(sys.modules['__main__'].__file__)
 def path(filename):
@@ -34,6 +36,70 @@ def load_pickle(file_name="card_dict.pkl"):
     except Exception as err:
         print("Error loading pickle")
         print(err)
+
+
+def save_settings(settings,filename="settings.txt"):
+    try:
+        settings
+        text= ""
+        for k,v in settings.items():
+            line = str(k)+" = "+str(v)+"\n"
+            text += line
+
+        with open(filename, "w") as text_file:
+            text_file.write(text)
+
+        return True
+    except Exception as err:
+        print("Error while saving settings file")
+        print(err)
+
+def load_settings(filename="settings.txt"):
+    #default settings
+
+    settings = {
+            'show_tier' : True,
+            'show_tier_rank' : False,
+            'show_win_rate' : True,
+            'show_pick_rate' : True,
+            'show_price' : True,
+            'flag_auto_scan':False,
+            'overlay_x':0,
+            'overlay_y':0,
+            'launcher_x':'default',
+            'launcher_y':'default'
+            
+    }
+    try:
+        if os.path.exists(filename) ==False:
+            print("Saving settings for the first time..")
+            save_settings(settings,filename=filename)
+        else:
+            with open(filename, "r") as ins:
+                for line in ins:
+                    if (line.startswith("#")==False) and  ("=" in line):
+                            if len(line.split('=')) == 2:
+                                prop,val  = [x.strip() for x in line.split('=')]
+
+                                if val.lower() == 'true':
+                                    settings[prop] = True
+                                elif val.lower() == 'false':
+                                    settings[prop] = False
+                                else:
+                                    try:
+                                        settings[prop] = int(val)
+                                    except ValueError:
+                                        settings[prop] = val
+
+
+
+        return settings
+    except Exception as err:
+        print("Error loading settings file.")
+        print(err)
+        return settings
+
+
 
 def fix_dict(d):
     '''...And One for Me   ->    ...And One For Me'''
@@ -61,13 +127,17 @@ label_list = []
 flag_swap = 1
 coisas = []
 
-launcher_x, launcher_y =  0, 0
-overlay_x, overlay_y = 0, 0
+settings = load_settings()
+
+launcher_x, launcher_y =  settings['launcher_x'], settings['launcher_y']
+overlay_x, overlay_y = settings['overlay_x'], settings['overlay_y']
+flag_auto_scan = settings['flag_auto_scan']
+
 x_drag, y_drag = None, None
 
 flag_auto_hide = False
 
-flag_auto_scan = False
+
 
 bg_color = 'magenta'
 
@@ -75,6 +145,7 @@ btn_auto_scan = None
 
 ss_path = path('screenshot_draft.png')
 custom_grid_path = path("custom_grid.npy")
+    
 
 def compare_images(img, img2):
     dif = np.mean(np.abs(img.astype(int) - img2.astype(int)))
@@ -184,7 +255,7 @@ def btnProcessScreen(ll_cur, root, screen_width, screen_height, auto_scan=False)
     #save_debugg_screenshot(ss, card_grid, borders)
     for row in range(2):
         for col in range(6):
-            #quatro cantos da carta
+            #four corners of the card
             top, left, bottom, right = card_grid[row, col, :]
             card_name = fix_dict(cards[row*6+col])
             card_score = scores[row*6+col]
@@ -203,9 +274,19 @@ def btnProcessScreen(ll_cur, root, screen_width, screen_height, auto_scan=False)
                 pr = None
 
             try:
-                tier = str(tiers[card_name])[0]
+                tier = str(tiers[card_name].tier)[0]
             except:
                 tier = '?'
+
+            try:
+                tier_rank = str(tiers[card_name].tier_rank)
+            except:
+                tier_rank = '?'
+
+            try:
+                custom_annotation = str(tiers[card_name].custom)
+            except:
+                custom_annotation = ''
 
             try:
                 price = str(prices[card_name]['sell_price'])
@@ -227,15 +308,30 @@ def btnProcessScreen(ll_cur, root, screen_width, screen_height, auto_scan=False)
                 label.place(anchor=anchor, x=x if x is not None else right - margin, y=y if y is not None else top + margin)
                 ll.append(label)
 
-            if len(tier) > 0:
-                add_label(tier, x=right - margin,  y=top - 15, anchor='e', color=tier_colors[tier[0]])
+            if settings['show_tier'] == False:
+                tier = ''
+            if settings['show_tier_rank'] == True:
+                tier += "("+tier_rank+")"
 
-            add_label('w {:.1f}%'.format(wr), y=yy, color=win_rate_colors[wr])
-            yy += lh
+            if settings['show_tier'] == True:
+                if len(tier) > 0:
+                    add_label(tier, x=right - margin,  y=top - 15, anchor='e', color=tier_colors[tier[0]])
 
-            add_label('p {:.1f}%'.format(pr), y=yy, color=pick_rate_colors[pr])
-            yy += lh
-            add_label('{:s}'.format(price), y=yy, color=price_colors[price])
+            if settings['show_win_rate'] == True:
+                add_label('w {:.1f}%'.format(wr), y=yy, color=win_rate_colors[wr])
+                yy += lh
+
+            if settings['show_pick_rate'] == True:
+                add_label('p {:.1f}%'.format(pr), y=yy, color=pick_rate_colors[pr])
+                yy += lh
+
+            if settings['show_price'] == True:
+                add_label('{:s}'.format(price), y=yy, color=price_colors[price])
+                yy += lh
+
+            if len(custom_annotation) > 0:
+                add_label(custom_annotation, y=yy)
+                yy += lh
 
     destroy_list(ll_cur, root)
     for ele in ll:
@@ -452,15 +548,32 @@ def swap_window(root, screen_width, screen_height, first_time=False):
         
         flag_swap = 0
 
+def save_settings_on_exit():
+    print("Saving and closing")
+    global settings, overlay_x,overlay_y
+    settings['overlay_x'] = overlay_x
+    settings['overlay_y'] = overlay_x
+    save_settings(settings)
+    return True
+
 def main():
-    global launcher_x , launcher_y
+    global launcher_x , launcher_y, settings
+
+    atexit.register(save_settings_on_exit)
 
     root = tk.Tk()
     root.iconbitmap(path('favicon.ico'))
 
     screen_width = root.winfo_screenwidth() # width of the screen
     screen_height = root.winfo_screenheight() 
-    launcher_x , launcher_y = screen_width/2-400,screen_height/2-300
+
+    if isinstance(settings['launcher_x'],int) == False:
+        launcher_x = screen_width/2-400
+        #settings['launcher_x'] = launcher_x
+
+    if isinstance(settings['launcher_y'],int) == False:
+        launcher_y = screen_height/2-300
+        #settings['launcher_y'] = launcher_x
     
     swap_window(root, screen_width, screen_height, first_time=True)
     
